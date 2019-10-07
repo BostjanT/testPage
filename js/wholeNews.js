88e9ff844895cd83e79129e0998ca80a2bb5905d
BostjanT <36411197+BostjanT@users.noreply.github.com>
2019-10-07 08:37:39 +0200

    Add files via upload

--- js/wholeNews.js
+++ js/wholeNews.js
@@ -1,3 +1,5 @@
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
+
 const client = contentful.createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
   space: "quuudgeu6kvg",
@@ -7,7 +9,7 @@ const client = contentful.createClient({
 
 // varibales
 
-const displayNews = document.querySelector(".news-center");
+const displayNews = document.querySelector(".display-news");
 
 // getting the news
 class News {
@@ -16,22 +18,20 @@ class News {
       let contentful = await client.getEntries({
         content_type: "klubskeNovice"
       });
-      console.log(contentful);
 
-      /*  let result = await fetch("news.json");
+      /* let result = await fetch("news.json");
       let data = await result.json(); */
-
-      /* let news = data.news; */
       let news = contentful.items;
+      /* let news = data.news; */
       news.sort(function(a, b) {
         return b - a;
       });
 
       news = news.map(item => {
-        const { title, besedilo } = item.fields;
+        const { title, date } = item.fields;
+        const { besedilo } = item.fields;
         const image = item.fields.image.fields.file.url;
-
-        return { title, besedilo, image };
+        return { title, besedilo, image, date };
       });
       return news;
     } catch (error) {
@@ -45,22 +45,22 @@ class UI {
     let result = "";
     news.forEach(news => {
       result += `
-         <article class="novica">
-      <div class="img-container">
-        <img 
-        src="${news.image}" 
-        alt="news image"
-        class="news-img">
-        <button class="news-btn" data-id="1">
-        <a href="news.html">
-          <i class="fas fa-book-reader">
-          <span class="space">Preberite več<span></i>
-         </a> 
-        </button>
-      </div>
-      <h3 class="naslov font-weight-bold">${news.title}</h3>
-     
-    </article>
+      
+            <div class="blog-content">
+              <div class="img-container">
+                <img src="${news.image}" alt="novica" />
+              </div>
+              <div class="date">${news.date}</div>
+              <div class="post-title my-2 text-center font-weight-bold">
+               ${news.title}
+              </div>
+              <div>
+              <p class="post-content mb-5">
+                ${documentToHtmlString(news.besedilo)}
+              </p>
+              </div>
+            </div>
+        
     
 
     `;
@@ -91,3 +91,15 @@ document.addEventListener("DOMContentLoaded", () => {
     Storage.saveNews(news);
   });
 });
+
+/*  <div class="carousel-item col-md-3 active">
+        <div class="blog-thumbnail">
+        <a href="#"><img src="${news.image}" class="img-fluid mx-auto d-block" alt="slide 1"></a>
+        <div class="blog-content">
+          <span class="post-date">${news.title}</span>
+          <p class="post-title">${news.besedilo}</p>
+        </div>
+
+        </div>
+
+      </div> */
